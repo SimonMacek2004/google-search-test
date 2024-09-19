61abd35c80aa8ec3a9fe71104c88e8401d5f1fc7
SimonMacek2004 <smacek04@gmail.com>
2024-09-19 20:43:17 +0200

    Initial commit with backend and frontend

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/search', async (req, res) => {
+    const { query } = req.body;
+    const { language } = req.body
+
+    try {
+        const response = await axios.get('https://www.googleapis.com/customsearch/v1', {
+            params: {
+                q: query,
+                key: process.env.API_KEY,  
+                cx: process.env.CX,
+                start: 1,
+                lr: language
+            },
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error fetching search results:', error);
+        res.status(500).json({ error: 'Failed to fetch search results' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});

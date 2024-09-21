f21b7991c151aaa88c2f8aadd70a8a358e44e179
SimonMacek2004 <smacek04@gmail.com>
2024-09-21 11:13:44 +0200

    Delete localhost from log

--- backend/server.js
+++ backend/server.js
@@ -37,5 +37,5 @@ app.post('/search', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
